f661b8b2cd51822aef30ead46d430ee70a4b95ee
vbrunoro <vfmbrunoro@gmail.com>
2024-06-19 16:09:31 -0300

    Fix pagination

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,9 @@
+import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
+import { getOrders } from '@/api/get-orders'
 import { Pagination } from '@/components/pagination'
 import {
   Table,
@@ -10,9 +14,29 @@ import {
 } from '@/components/ui/table'
 
 import { OrderTableFilters } from './order-table-filters'
-import { OrderTableRow } from './order-table-tow'
+import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
+  const { data: result } = useQuery({
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
+  })
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((state) => {
+      state.set('page', (pageIndex + 1).toString())
+
+      return state
+    })
+  }
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -39,12 +63,20 @@ export function Orders() {
               </TableHeader>
 
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return <OrderTableRow key={i} />
-                })}
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
               </TableBody>
             </Table>
-            <Pagination pageIndex={0} totalCount={105} perPage={10} />
+            {result && (
+              <Pagination
+                onPageChange={handlePaginate}
+                pageIndex={result.meta.pageIndex}
+                totalCount={result.meta.totalCount}
+                perPage={result.meta.perPage}
+              />
+            )}
           </div>
         </div>
       </div>
