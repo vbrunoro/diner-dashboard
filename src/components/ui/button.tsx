0f4925b56b4a3f9b523f03652679580f31e7dd8d
vbrunoro <vfmbrunoro@gmail.com>
2024-06-18 22:54:23 -0300

    Add restaurant profile edit

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,6 +16,8 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        success:
+          'bg-emerald-500 text-white hover:bg-emarald-500/90 dark:bg-emerald-600 dark:hover:bg-emerald-600/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
